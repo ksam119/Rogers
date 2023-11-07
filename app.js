a1e11877bc9a49921ff5c719a3089b1aa91c7c61
ksam119 <150139569+ksam119@users.noreply.github.com>
2023-11-07 03:46:02 -0800

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_cczcb0i", "template_k7ixms9", formData)
+    emailjs.send("service_azq4gfn", "template_s7uy9py", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
